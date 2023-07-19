3b3ea4020005d3c7417b46f9c04c117fd6ee9dbf
mohammed <mohammedrashaideh23@gmail.com>
2023-07-18 22:07:28 -0500

    changing feign client dependency

--- Avatar/src/main/java/com/SA/Avatar/AvatarApplication.java
+++ Avatar/src/main/java/com/SA/Avatar/AvatarApplication.java
@@ -4,7 +4,7 @@ import com.SA.Avatar.Entity.Element;
 import com.SA.Avatar.Entity.ElementType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
--- Avatar/src/main/java/com/SA/Avatar/Client/StudentClient.java
+++ Avatar/src/main/java/com/SA/Avatar/Client/StudentClient.java
@@ -1,13 +1,13 @@
 package com.SA.Avatar.Client;
 
 import com.SA.Avatar.DTO.StudentDTO;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("StudentService")
+@FeignClient("student-service")
 public interface StudentClient {
     @GetMapping("/students/{id}")
     public StudentDTO getStudent(@PathVariable String id);
