100d0ca7cf585feb845f1ffd552fbfed19426001
ntehah <ntehah.kdr07@gmail.com>
2023-07-19 21:53:34 -0500

    adding smtp server

---
+++ smtp-server/src/test/java/com/SA/SmtpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
